3c300462a456c7ad3d55d2634a5a071586f82b29
Andreas Madsen <amwebdk@gmail.com>
2016-03-14 14:49:53 +0100

    [test] add value tests for database

--- code/test/database.py
+++ code/test/database.py
@@ -5,4 +5,20 @@ from nose.tools import *
 import dataset
 
 database = dataset.Database.from_id(1)
+
+assert_equal(database.courses, 30)
+assert_equal(database.rooms, 6)
+assert_equal(database.days, 5)
+assert_equal(database.periods_per_day, 6)
+assert_equal(database.curricula, 14)
+assert_equal(database.constraints, 53)
+assert_equal(database.lecturers, 24)
+
+assert_equal(database.execute('select count(*) from courses').fetchone(), (30, ))
+assert_equal(database.execute('select count(*) from curricula').fetchone(), (14, ))
+assert_equal(database.execute('select count(*) from lecturers').fetchone(), (24, ))
+assert_equal(database.execute('select count(*) from relation').fetchone(), (42, ))
+assert_equal(database.execute('select count(*) from rooms').fetchone(), (6, ))
+assert_equal(database.execute('select count(*) from unavailability').fetchone(), (53, ))
+
 database.close()
