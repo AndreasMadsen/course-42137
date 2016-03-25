64e7b8b5981a1245bb57ab5ed71c412ad6c7325a
Andreas Madsen <amwebdk@gmail.com>
2016-03-25 12:24:10 +0100

    [test] rename swap tests

--- code/test/solution_mutate_swap.py
+++ code/test/solution_mutate_swap.py
@@ -19,55 +19,18 @@ orignal_schedule = sorted([
     (2, 1, 0, 0)
 ])
 
-def test_remove_failure():
-    s = solution.Solution(database, list(orignal_schedule))
-
-    # missing, right failure
-    assert_equal(s.simulate_swap((2, 0, 0, 0), (1, 4, 0, 0)), None)
-    assert_equal(sorted(s.export()), orignal_schedule)
-
-    # missing, left failure
-    assert_equal(s.simulate_swap((1, 4, 0, 0), (2, 0, 0, 0)), None)
-    assert_equal(sorted(s.export()), orignal_schedule)
-
-def test_add_failure():
-    s = solution.Solution(database, list(orignal_schedule))
-
-    # Insert course 0 on (4, 0, 0), left failure
-    assert_equal(s.simulate_swap((0, 0, 0, 0), (1, 4, 0, 0)), None)
-    assert_equal(sorted(s.export()), orignal_schedule)
-
-    # Insert course 0 on (4, 0, 0), right failure
-    assert_equal(s.simulate_swap((1, 4, 0, 0), (0, 0, 0, 0)), None)
-    assert_equal(sorted(s.export()), orignal_schedule)
-
-def test_redudant_swap():
-    s = solution.Solution(database, list(orignal_schedule))
-
-    # Swap course 0 with 0 should work but be redundant
-    assert_equal(s.simulate_swap((0, 0, 0, 0), (0, 0, 1, 0)), 0)
-    assert_equal(sorted(s.export()), orignal_schedule)
-
 def test_working_swap():
     # More working days for course 1
     s = solution.Solution(database, list(orignal_schedule))
+    old_penalties = dict(s.penalties)
+    old_objective = s.objective
 
     # Swap left
-    assert_equal(s.simulate_swap((2, 1, 0, 0), (1, 4, 2, 0), full=True), {
-        'U_sum': 0,
-        'W_sum': -1,
-        'P_sum': 0,
-        'A_sum': 0,
-        'V_sum': 0
-    })
-    assert_equal(sorted(s.export()), orignal_schedule)
-
+    s.mutate_swap((2, 1, 0, 0), (1, 4, 2, 0))
     # Swap right
-    assert_equal(s.simulate_swap((1, 4, 2, 0), (2, 1, 0, 0), full=True), {
-        'U_sum': 0,
-        'W_sum': -1,
-        'P_sum': 0,
-        'A_sum': 0,
-        'V_sum': 0
-    })
+    s.mutate_swap((2, 4, 2, 0), (1, 1, 0, 0))
+
+    # Expect same schedule
     assert_equal(sorted(s.export()), orignal_schedule)
+    assert_equal(s.penalties, old_penalties)
+    assert_equal(s.objective, old_objective)
