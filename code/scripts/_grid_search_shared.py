d992f2d91763ebca32f99c5538395a917b906119
Andreas Madsen <amwebdk@gmail.com>
2016-04-06 13:19:59 +0200

    [refactor] seperate alns and tabu grid search

---
+++ code/scripts/_grid_search_shared.py
@@ -0,0 +1,12 @@
+
+import solution
+import dataset
+
+databases = [
+    dataset.Database.from_id(db_id) for db_id in range(1, 14, 2)
+]
+
+def initalizer(database):
+    initial_solution = solution.Solution(database)
+    solution.initialize(initial_solution)
+    return initial_solution
--- code/scripts/grid_search_tabu.py
+++ code/scripts/grid_search_tabu.py
@@ -1,5 +1,6 @@
 
 from _setup import results_path
+from _grid_search_shared import databases, initalizer
 
 import collections
 import pickle
@@ -11,21 +12,6 @@ import solution
 import dataset
 import search
 
-databases = [
-    dataset.Database.from_id(1),
-    dataset.Database.from_id(2)
-]
-
-def initalizer(database):
-    initial_solution = solution.Solution(database)
-    solution.initialize(initial_solution)
-    return initial_solution
-
-alns_parameters = collections.OrderedDict([
-    ('update_lambda', [0.8, 0.9, 0.95, 0.99]),
-    ('remove', [1, 5])
-])
-
 tabu_parameters = collections.OrderedDict([
     ('diversification', [True, False]),
     ('intensification', [True, False]),
@@ -35,11 +21,6 @@ tabu_parameters = collections.OrderedDict([
 
 grid = gridsearch.GridSearch(databases, initalizer, time=3 * 60,
                              verbose=True)
-
-alns_results = grid.search(search.ALNS, alns_parameters)
-np.save(results_path('alns.npy'), alns_results, allow_pickle=False)
-with open(results_path('alns.plk'), 'wb') as f:
-    pickle.dump(alns_parameters, f, protocol=pickle.HIGHEST_PROTOCOL)
 
 tabu_results = grid.search(search.TABU, tabu_parameters)
 np.save(results_path('tabu.npy'), tabu_results, allow_pickle=False)
